231a6d89cb4e75ce1f9a66412399f8548b61af0a
Jakub Matwiejew <custom@audiocustom.pl>
2018-01-30 21:54:57 +0100

    Dzień 2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,13 @@
 //Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) => {
+    if (err === null){
+        console.log('Poprawnie odczytano tablicę:', data);
+        const arr = JSON.parse(data);
+        let sum = arr.reduce(function (a, b) {return a + b;}, 0);
+        console.log(`Suma elementów w talicy to: ` + sum);
+    } else {
+        console.log(`Błąd...`, err)
+    }
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,22 @@
 //Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanieDnia/test.txt', 'utf8', (err, data) => {
+    if (err === null){
+        let text = data;
+        let res = text.split('').map(function(v,i){
+            return i%2 === 0 ? v.toLowerCase():v.toUpperCase()
+        });
+        console.log(res.join(''));
+        fs.writeFile('./data/zadanieDnia/test.txt', res.join(''), err => {
+            if (err === null){
+                console.log('Zapisano poprawnie!');
+            } else {
+                console.log('Błąd podczas zapisu pliku!', err);
+            }
+        });
+
+    } else {
+        console.log(`Błąd...`, err)
+    }
+});
