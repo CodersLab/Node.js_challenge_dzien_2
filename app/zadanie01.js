3e427cc9d27a06f405972eb4fbd64742a3b76a7f
Maciej Judka <maciej.judka@gmail.com>
2018-01-30 17:31:11 +0100

    Add zadanie01

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,11 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf-8', (err, data) => {
+	if (err) return console.log('Błąd odczytu pliku!');
+	
+	let sum = JSON.parse(data).reduce((x, y) => x + y);
+	
+	fs.writeFile('./data/zadanie01/sumss.txt', sum, err => {
+		if (err) return console.log('Błąd zapisu pliku!')
+	});
+});
