d2a2c5d8f59ce44e8b78ef28deac455d5ba180ac
Łukasz Bułka <mckornik@poczta.fm>
2018-01-30 22:54:01 +0000

    Day 2

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,27 @@
 //Twój kod
+
+const fs = require('fs');
+const fileName = process.argv[2];
+
+fs.readFile('./data/zadanieDnia/' + fileName, 'utf8', (err, data) => {
+   if (err === null){
+       console.log('Odczytalem plik.');
+       let file = '';
+      for (let i = 0; i < data.length; i++){
+          if (i % 2){
+              file += data[i].toLowerCase();
+          } else {
+            file += data[i].toUpperCase();
+          }
+      }
+    fs.writeFile('./data/zadanieDnia/' + fileName, file, err => {
+       if (err === null){
+           console.log('Zapisalem plik.');
+       } else {
+           console.log('Kurza twarz, cos poszlo nie tak.', err);
+       }
+    });
+   } else {
+       console.log('Motyla noga! Nie udalo mi sie odczytac pliku', err);
+   }
+});
