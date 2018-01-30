767e50d7ff2174c1ba5f64cf3b050d9a4e692dae
RadekM86 <byrm.design@gmail.com>
2018-01-30 18:53:03 +0100

    Zadanie 1

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,19 @@
 //Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) => {
+    if (err === null){ 
+        console.log('Poprawnie odczytano plik.', data);
+        let dataParsed = JSON.parse(data);
+        var sum = dataParsed.reduce((prev,curr)=>{return prev+curr})
+        fs.writeFile('./data/zadanie01/sum.txt', sum, err => {
+            if (err === null){
+                console.log("Sukces!");
+    } else { 
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+    });
+    }else{ 
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
