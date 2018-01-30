00408271a521b51bbdbac829bc6ebec764ee07ed
Łukasz Dadlez <lukaszdadlez@gmail.com>
2018-01-30 23:36:40 +0100

    rozwiązania zadań

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,24 @@
 //Twój kod
+const fs = require('fs');
+
+const path = './data/zadanie02/';
+
+const readContent = (filePath) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (!err) {
+      console.log(filePath.includes('.json') ? JSON.parse(data) : data);
+    } else {
+      console.log(`Błąd podczas czytania pliku:\n`, err);
+    }
+  });
+}
+
+fs.readdir(path, (err, files) => {
+  if (!err) {
+    files.forEach(file => {
+      readContent(path + file);
+    });
+  } else {
+    console.log('Błąd podczas czytania listy plików.\n', err);
+  }
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,26 @@
 //Twój kod
+const fs = require('fs');
+
+const defaultFilePath = './data/zadanieDnia/test.txt';
+const filePath = process.argv[2];
+const outputPath = './data/zadanieDnia/output.txt'
+
+const setCase = (letter, index) => {
+  return index % 2 != 0 ? letter.toUpperCase() : letter.toLowerCase();
+}
+
+const transformText = (text) => {
+  let result = '';
+  for (let i = 0, len = text.length; i < len; i++) { //czy jest zgrabniejsza metoda iterowania po tekście pozwalająca przekazać indeks litery?
+    result += setCase(text[i], i);
+  }
+  return result;
+}
+
+fs.readFile(filePath || defaultFilePath, 'utf8', (err, data) => {
+  if (!err) {
+    fs.writeFile(outputPath, transformText(data), err => console.log(!err ? 'Posiano trawę tutaj: ' + outputPath : 'Błąd podczas zapisywania wyniku do pliku:\n' + err));
+  } else {
+    console.log('Błąd podczas czytania pliku:\n', err);
+  }
+});
