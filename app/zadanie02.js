b98f092b376ddb9de4ea8026f4246326bfa85a97
Filip Godziński <godzinski.filip@gmail.com>
2018-01-30 21:40:12 +0100

    Zadanie 02 - done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,19 @@
 //Twój kod
+
+const fs = require('fs');
+
+fs.readdir('./data/zadanie02', (err, files) => {
+    if (err === null){
+        files.forEach(file => {
+            fs.readFile(`./data/zadanie02/${file}`, 'utf8', (err, data) => {
+                if (err === null){
+                    console.log(`Poprawnie odczytano plik ${file}. Jego zawartość to: `, data);
+                } else {
+                    console.log('Błąd podczas odczytu pliku!', err);
+                }
+            });
+        });
+    } else {
+        console.log('Błąd podczas listowania katalogu!', err);
+    }
+});
