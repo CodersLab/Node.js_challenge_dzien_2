ea3b6adec18c8ed85a024e49c3fd31811bb5671d
RadekM86 <byrm.design@gmail.com>
2018-01-30 19:29:56 +0100

    Zadadnie 2 i zadanie dnia zrobione

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,14 @@
 //Twój kod
+const fs = require('fs');
+
+//Odczytaj listę plików i folderów...
+fs.readdir('./data/zadanie02/', (err, files) => {
+    if (err === null){
+        console.log('Lista plików:');
+        files.forEach(file => {
+            console.log(file);
+        });
+    } else {
+        console.log('Błąd podczas listowania katalogu!', err);
+    }
+});
