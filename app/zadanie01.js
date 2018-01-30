9ce4d5ca16a0d9e1218bcc595866bf6e4a912a18
Maciej Woźniak <mwoz@softwebo.private>
2018-01-30 17:10:15 +0100

    fun with JS #2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,21 @@
 //Twój kod
+
+const fs = require("fs");
+
+fs.readFile("./data/zadanie01/input.json", "utf-8", (err, data) => {
+    if (err === null){
+        let sum = 0;
+        JSON.parse(data).forEach(a => sum += a);
+        fs.writeFile("./data/zadanie01/sum.txt", sum, err => {
+            if (err === null) {
+                console.log("Zapisano wynik!");
+            }
+            else {
+                console.log('Błąd podczas zapisu pliku!', err);
+            }
+        });
+    }
+    else {
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,26 @@
 //Twój kod
+
+const fs = require("fs");
+const dir = "./data/zadanie02/";
+
+fs.readdir(dir, (err, files) => {
+   if (err === null) {
+       files.forEach(file => readFile(dir+file));
+   }
+   else {
+       console.log("Nie udało się przeczytać katalogu: " + err);
+   }
+
+});
+
+
+function readFile(file) {
+    fs.readFile(file, "utf-8", (err, data) => {
+        if (err === null) {
+            console.log(data);
+        }
+        else {
+            console.log("Nie udało się odczytac pliku: " + file + "! " + err);
+        }
+    });
+}
