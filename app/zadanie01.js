af03ca1d1b02058884bb24015c46a2d57502cb68
Maciej Judka <maciej.judka@gmail.com>
2018-01-30 17:35:14 +0100

    Update zadanie01

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -5,7 +5,8 @@ fs.readFile('./data/zadanie01/input.json', 'utf-8', (err, data) => {
 	
 	let sum = JSON.parse(data).reduce((x, y) => x + y);
 	
-	fs.writeFile('./data/zadanie01/sumss.txt', sum, err => {
-		if (err) return console.log('Błąd zapisu pliku!')
+	fs.writeFile('./data/zadanie01/sum.txt', sum, err => {
+		let msg = err ? 'Błąd zapisu pliku' : 'Plik poprawnie zapisany';
+		return console.log(msg)
 	});
 });
