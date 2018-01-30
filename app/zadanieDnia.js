9566712d60be75bb75152886629b6e32d097a1b2
Maciej Judka <maciej.judka@gmail.com>
2018-01-30 18:37:19 +0100

    Add zadanieDnia

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,17 @@
-//Twój kod
+const fs = require('fs');
+
+const path = process.argv.slice(2).toString();
+
+let trawka = (path) => {
+	fs.readFile(path, 'utf-8', (err, data) => {
+		if (err) return console.log('Błąd odczytu pliku');
+		
+		let newData = [...data].map((elem, i) => {
+			return i % 2 === 0 ? elem.toUpperCase() : elem
+		})
+		
+		console.log(newData.join(''))
+	});
+};
+
+trawka(path)
