4abac0630ce5eada86e09ebbd1a693e0ca8f360c
Filip Godziński <godzinski.filip@gmail.com>
2018-01-31 11:47:27 +0100

    Zadanie 01 - done

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -4,13 +4,12 @@ const fs = require('fs');
 
 fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) => {
     if (err === null){
-        console.log('Poprawnie odczytano plik.', data);
         let arrParsed = JSON.parse(data);
         let sum = arrParsed.reduce( (prev, curr) => { return prev + curr } );
 
         fs.writeFile('./data/zadanie01/sum.txt', sum, err => {
             if (err === null){
-                console.log('Zapisano poprawnie!');
+                console.log('Poprawnie odczytano i zapisano plik! Suma wynosi: ', sum);
             } else {
                 console.log('Błąd podczas zapisu pliku!', err);
             }
