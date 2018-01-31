f0fb06043ccda09b4ea963c57674e89a4132a46c
Gladkowski <marcingladkowski@gmail.com>
2018-02-01 00:29:48 +0100

    Day 2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,38 @@
-//Twój kod
+const fs = require('fs');
+
+let read = () => {
+
+    return new Promise((resolve, reject) => {
+
+    fs.readFile('./data/zadanie01/input.json', 'utf-8', (err, data) => {        
+            if (err){
+                throw err;
+            } else {
+                resolve(data);    
+            }
+        });
+    });
+}
+
+let sum = (data) => {
+
+        let dataArr = JSON.parse(data);
+        let sum = 0;
+        
+        for(index in dataArr){ 
+            sum += dataArr[index] 
+        }
+        return sum;
+}
+
+let write = (sum) => {
+
+    fs.writeFile('./data/zadanie01/sum.txt', sum, (err) => {
+        if (err) throw err;
+    });
+
+}
+
+read()
+.then((data) => { return sum(data) })
+.then((sum) => { write(sum) } );
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,38 @@
-//Twój kod
+const fs = require('fs');
+
+let read = () => {
+
+    return new Promise((resolve, reject) => {
+
+        fs.readdir('./data/zadanie02/', (err, files) => {
+            if (err === null){
+                resolve(files)
+            } else {
+                throw err;
+            }
+        });
+    });    
+}
+
+let content = (files) => {
+        files.forEach(file => {
+            getContent(file);
+        });
+}
+
+let getContent = (file) => {
+
+    let fileDir = `./data/zadanie02/${file}`
+
+    fs.readFile(fileDir, 'utf-8', (err, data) => {        
+        if (err){
+            throw err;
+        } else {
+            console.log(data); 
+        }
+    });
+
+}
+
+read().then((data) => { content(data) })
+
