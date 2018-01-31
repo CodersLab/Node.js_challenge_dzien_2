abb3c3a5d5020c88f86b85c6f2f865061ccb7cfa
RadekM86 <byrm.design@gmail.com>
2018-01-31 18:40:59 +0100

    Poprawka w zad 2

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -4,9 +4,14 @@ const fs = require('fs');
 //Odczytaj listę plików i folderów...
 fs.readdir('./data/zadanie02/', (err, files) => {
     if (err === null){
-        console.log('Lista plików:');
-        files.forEach(file => {
-            console.log(file);
+        files.forEach( file => {
+            fs.readFile(`./data/zadanie02/${file}`, 'utf8', (err, data) => {
+                if (err === null){
+                    console.log(`Poprawnie odczytano plik ${file}. Dane z pliku: `, data);
+                } else {
+                    console.log('Błąd podczas odczytu pliku!', err);
+                }
+            });
         });
     } else {
         console.log('Błąd podczas listowania katalogu!', err);
