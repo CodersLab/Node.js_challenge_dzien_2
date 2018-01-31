23f56fc14fe06a0a76f353a01c37566ba5e74c27
Jakub Stephan <j.stephan@cctechnology.pl>
2018-01-31 10:42:06 +0100

    work day 2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,18 @@
-//Twój kod
+const fs = require('fs');
+const path = './data/zadanie01';
+fs.readFile(`${path}/input.json`, 'utf8', (error, data) => {
+    if (error) {
+        console.error('Błąd podczas odczytu pliku!', error);
+    } else {
+        const arrayOfNumbers = JSON.parse(data);
+        let arraySum = 0;
+        arrayOfNumbers.forEach((value) => arraySum += value);
+        fs.writeFile(`${path}/sum.txt`, arraySum, err => {
+            if (err === null){
+                console.info('Zapisano poprawnie!');
+            } else {
+                console.error('Błąd podczas zapisu pliku!', err);
+            }
+        });
+    }
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,23 @@
 //Twój kod
+const fs = require('fs');
+const fileName = process.argv[2];
+const path = `./data/zadanieDnia/${fileName}`;
+
+fs.readFile(path, 'utf8', (error_read, data) => {
+    if (error_read === null){
+        let content = '';
+        for (let i = 0; i < data.length; i += 1){
+            console.log('znak data[i] =', data[i]);
+            content += (i % 2 === 0) ? data[i].toUpperCase() : data[i].toLowerCase();
+        }
+        fs.writeFile(`./data/zadanieDnia/test2.txt`, content, error_write => {
+            if (error_write === null){
+                console.log('zapisno plik.');
+            } else {
+                console.log('blad zapisu:', error_write);
+            }
+        });
+    } else {
+        console.log('nie udalo sie odczytac pliku =', error_read);
+    }
+});
