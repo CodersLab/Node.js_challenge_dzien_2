a3befae623173c204d0255b901ecb1faf98e4352
Filip Godziński <godzinski.filip@gmail.com>
2018-01-31 11:50:03 +0100

    ZadanieDnia - done

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,24 @@
 //Twój kod
+
+const fs = require('fs');
+
+const textFile = process.argv[2];
+
+fs.readFile(textFile, 'utf8', (err, data) => {
+    if (err === null){
+        let trawkaText = "";
+        data.split("").map( (elem, index) => {
+            trawkaText += (index % 2 === 0) ? elem.toUpperCase() : elem.toLowerCase();
+        });
+
+        fs.writeFile(textFile, trawkaText, err => {
+            if (err === null){
+                console.log('Poprawnie odczytano i zapisano plik');
+            } else {
+                console.log('Błąd podczas zapisu pliku!', err);
+            }
+        });
+    } else {
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
