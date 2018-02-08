95a171791df531ee8b77629fee77e35decc4e013
Gosia Malinowska <malinowska.gosia6@gmail.com>
2018-02-09 00:53:35 +0100

    finish day2

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,12 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readdir('./data/zadanie02/', (err, files) => {
+    if (err === null) {
+        console.log('Lista plików: ');
+        files.forEach(file => {
+            console.log(file);
+        });
+    } else {
+        console.log('Błąd podczas listowania katalogu');
+    }
+    });
