7186b9cf043ef539bb326b85960962f49874b5ff
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-09 14:52:40 +0100

    Challenge 2 done

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -3,22 +3,15 @@ const fs = require('fs');
 function TrAwKa(fileName) {
     fs.readFile(fileName, 'utf8', (err, data) => {
         if (err === null) {
-            let text = data.split(" ");
-            for (let i = 0; i < text.length; i++){
-                text[i] = text[i].split("");
-                if (i%0 !== 2) {
-                    text[i][0] = text[i][0].toLowerCase()
-                }
-                else {
-                    text[i][0] = text[i][0].toUpperCase();
-                }
-
-                text[i] = text[i].join("");
-            }
-            return console.log(text.join(" "));
-
+            let text = '';
+            [...data].forEach((e, i) => {
+                text += (i % 2 === 0) ? e.toUpperCase() : e.toLowerCase();
+            });
+            fs.writeFile(fileName, text, err => {
+                err === null? console.log('Plik został zmieniony w nieskoszoną TrAwKę:' +'\n\n'+ text) : console.log('Błąd zapisu pliku')
+            })
         } else {
-            console.log('Błąd podczas odczytu pliku!', err);
+            console.log('Błąd odczytu pliku')
         }
     });
 }
