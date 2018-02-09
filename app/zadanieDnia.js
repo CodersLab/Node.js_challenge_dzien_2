ab105fb90ac2c4a554861235783a99d2140b4e47
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-09 13:07:28 +0100

    in progress

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,25 @@
-//Twój kod
+const fs = require('fs');
+
+function TrAwKa(fileName) {
+    fs.readFile(fileName, 'utf8', (err, data) => {
+        if (err === null) {
+            let text = data.split(" ");
+            for (let i = 0; i < text.length; i++){
+                text[i] = text[i].split("");
+                if (i%0 !== 2) {
+                    text[i][0] = text[i][0].toLowerCase()
+                }
+                else {
+                    text[i][0] = text[i][0].toUpperCase();
+                }
+
+                text[i] = text[i].join("");
+            }
+            return console.log(text.join(" "));
+
+        } else {
+            console.log('Błąd podczas odczytu pliku!', err);
+        }
+    });
+}
+TrAwKa('../app/data/zadanieDnia/test.txt');
