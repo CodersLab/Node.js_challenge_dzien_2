47b099bd1e0aec8a16e49368de3ef31cff969322
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-09 12:34:44 +0100

    Ex. 02 done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,19 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readdir('../app/data/zadanie02', (err, files) => {
+    if (err === null){
+    files.forEach(file => {
+        fs.readFile('../app/data/zadanie02/'+file, 'utf8', (err, data) => {
+            if (err === null){
+                console.log(data);
+            } else {
+                console.log('Błąd podczas odczytu pliku!', err);
+            }
+        });
+
+
+});
+} else {
+    console.log('Błąd podczas listowania katalogu!', err);
+}
+});
