0a6293f97e901a212816aee291e8702a92217bb6
vinilll <dzisvolodymyr@gmail.com>
2018-03-03 22:36:24 +0100

    Day2 done!

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,17 @@
 //Twój kod
+
+const fs = require("fs");
+
+fs.readFile('./data/zadanie01/input.json', (err, data) => {
+    if (err === null) {
+        let sum = JSON.parse(data).reduce((a,b)=>{return a+b;});
+        fs.writeFile('./data/zadanie01/sum.txt',sum, err => {
+            if (err===null){
+                console.log("Zapisano popranie!")
+            } else
+                console.log("Blad zapisu!")
+        })
+    } else
+        console.log('Blad odczytu')
+
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,12 @@
 //Twój kod
+
+const fs = require("fs");
+
+fs.readdir('./data/zadanie02',(err,files)=>{
+    if(err===null){
+        files.forEach(file =>{
+            console.log(file);
+        })
+    } else
+        console.log('Blad odczytu!')
+});
