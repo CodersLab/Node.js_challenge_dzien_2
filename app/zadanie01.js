37a4f2a73b523be22c724d47db296ccd0739b763
Magdalena Goralczyk <magda.g10@wp.pl>
2018-07-04 22:05:29 +0200

    zadanie 1

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,28 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readFile('/home/magdalena/Dokumenty/Coderslab/Dzien2/app/data/zadanie01/input.json', 'utf8', (err, data) => {
+if (err==null){
+console.log('Poprawnie odczytano plik', data);
+
+var jason= JSON.parse(data);
+var sum=0;
+
+jason.forEach(element => {
+    sum+=parseInt(element);
+    
+});
+console.log(sum);
+
+fs.writeFile('/home/magdalena/Dokumenty/Coderslab/Dzien2/app/data/zadanie01/sum.txt', sum, err => {
+    if (err === null) {
+        console.log('Zapisano poprawnie!');
+
+    } else {
+        console.log('Błąd podczas zapisu pliku', err);
+    }
+});
+}
+
+ else {
+console.log('Błąd podczas odczytu', err); }
+});
