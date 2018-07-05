b1c2a87d12b67b6d8b021522eef585091e8160d4
damianlis <lis.damian@yahoo.pl>
2018-07-05 20:26:35 +0200

    Finished

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,19 @@
-//Twój kod
+const fs = require("fs");
+
+fs.readdir("./app/data/zadanie02", (err, files) => {
+  if (err === null) {
+    console.log("Działa ok, oto lista plików: ");
+    files.forEach(file => {
+      console.log(file);
+      fs.readFile(`./app/data/zadanie02/${file}`, "utf8", (err, data) => {
+        if (err === null) {
+          console.log(data);
+        } else {
+          console.log("Coś chyba z tym readFilem :/");
+        }
+      });
+    });
+  } else {
+    console.log("Nie bangla :/");
+  }
+});
